1988fc2faded9e05772053a7ca3e76efa1151c51
syncoGitHub <78350570+syncoGitHub@users.noreply.github.com>
2022-01-30 16:04:15 +0100

    End of the assignment

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var futureYear = new Date().getFullYear() + parseInt(years)
     
+    result =  "If you deposit " + principal + ",\<br\>";
+    result += "at an interest rate of " + rate +"%\<br\>";
+    result += "You will receive an amount of " + interest + ",\<br\>";
+    result += "in the year " + futureYear + "\<br\>"
+
+    
+    document.getElementById("result").innerHTML=result;
+
 }
-        
+
+
+function updateRate() 
+{
+    var rateValue = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateValue+"%";
+}
